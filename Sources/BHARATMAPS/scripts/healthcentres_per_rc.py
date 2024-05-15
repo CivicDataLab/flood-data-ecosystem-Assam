076e08551c892cc4967596287482f46de327cded
Archit Dubey <pokemongochamp47@gmail.com>
2024-05-15 17:55:13 +0530

    Bharatmaps data for Assam

--- Sources/BHARATMAPS/scripts/healthcentres_per_rc.py
+++ Sources/BHARATMAPS/scripts/healthcentres_per_rc.py
@@ -4,9 +4,9 @@ import os
 
 cwd = os.getcwd()
 health_centres_gdf = gpd.read_file(cwd+'/Sources/BHARATMAPS/data/RawData/BharatMaps_HealthCenters.geojson')
-odisha_subdist_gdf = gpd.read_file(cwd+'/assets/subdistricts_479_odisha_osm.geojson')
+assam_rc_gdf = gpd.read_file(cwd+'/Maps/Assam_Revenue_Circles/assam_revenue_circle_nov2022.shp')
 
-health_centres_in_rcs = gpd.sjoin(odisha_subdist_gdf, health_centres_gdf.to_crs(odisha_subdist_gdf.crs), how="left", predicate="contains")
-health_centres_count = health_centres_in_rcs.groupby(['id','name']).size().reset_index(name='health_centres_count')
+health_centres_in_rcs = gpd.sjoin(assam_rc_gdf, health_centres_gdf.to_crs(assam_rc_gdf.crs), how="left", predicate="contains")
+health_centres_count = health_centres_in_rcs.groupby('object_id').size().reset_index(name='health_centres_count')
 
 health_centres_count.to_csv(cwd+'/Sources/BHARATMAPS/data/variables/healthcentres_per_rc.csv', index=False)
--- Sources/BHARATMAPS/scripts/schools_per_rc.py
+++ Sources/BHARATMAPS/scripts/schools_per_rc.py
@@ -4,9 +4,9 @@ import os
 
 cwd = os.getcwd()
 schools_gdf = gpd.read_file(cwd+'/Sources/BHARATMAPS/data/RawData/BharatMaps_Schools.geojson')
-odisha_subdist_gdf = gpd.read_file(cwd+'/assets/subdistricts_479_odisha_osm.geojson')
+assam_rc_gdf = gpd.read_file(cwd+'/Maps/Assam_Revenue_Circles/assam_revenue_circle_nov2022.shp')
 
-schools_in_rcs = gpd.sjoin(odisha_subdist_gdf, schools_gdf.to_crs(odisha_subdist_gdf.crs), how="left", predicate="contains")
-schools_count = schools_in_rcs.groupby(['id','name']).size().reset_index(name='schools_count')
+schools_in_rcs = gpd.sjoin(assam_rc_gdf, schools_gdf.to_crs(assam_rc_gdf.crs), how="left", predicate="contains")
+schools_count = schools_in_rcs.groupby('object_id').size().reset_index(name='schools_count')
 
 schools_count.to_csv(cwd+'/Sources/BHARATMAPS/data/variables/Schools.csv', index=False)
