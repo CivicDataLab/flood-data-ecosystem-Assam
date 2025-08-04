b395b71a02793acc69dfe097dded6fde4fb2daff
saurabhlevin <saurabh.levin@gmail.com>
2025-08-04 09:43:21 +0530

    Update: tenders until July 2024

--- Sources/TENDERS/scripts/scraper/run_scraper.py
+++ Sources/TENDERS/scripts/scraper/run_scraper.py
@@ -7,7 +7,7 @@ script_path = cwd+r'\Sources\TENDERS\scripts\scraper\scraper_assam_recent_tender
 
 for year in range(2025,2026):
     year = str(year)
-    for month in range(5,9):        
+    for month in range(7,8):        
         month=str(month)
         print(year+'_'+month)
         subprocess.call([r"C:\Users\saura\anaconda3\envs\cdl-env\python.exe", script_path, year, month])
