a77d7b36b168a46ccd982e23a4f6790fc8bf73fa
saurabhlevin <saurabh.levin@gmail.com>
2026-04-06 16:18:25 +0530

    syncing with remote

--- Sources/TENDERS/scripts/scraper/run_scraper.py
+++ Sources/TENDERS/scripts/scraper/run_scraper.py
@@ -5,9 +5,9 @@ from datetime import date, timedelta
 cwd = os.getcwd()
 script_path = cwd+r'/Sources/TENDERS/scripts/scraper/scraper_assam_recent_tenders_tender_status.py'
 
-for year in range(2025,2026):
+for year in range(2026,2026):
     year = str(year)
-    for month in range(9,11):        
+    for month in range(1,4):        
         month=str(month)
         print(year+'_'+month)
         #subprocess.call([r"/opt/anaconda3/bin/python3", script_path, year, month])
